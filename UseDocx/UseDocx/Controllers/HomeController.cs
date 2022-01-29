4bd246ea592e1ecaaa339183f2e0afb9a4bb8d99
jontcont <52921053+JontCont@users.noreply.github.com>
2022-01-29 23:04:25 +0800

    add Docx

--- UseDocx/UseDocx/Controllers/HomeController.cs
+++ UseDocx/UseDocx/Controllers/HomeController.cs
@@ -1,27 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using UseDocx.Models;
+using Xceed.Words.NET;
 
 namespace UseDocx.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _env;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
         {
             _logger = logger;
+            _env = env;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            string docxPath = _env.WebRootPath+ "\\upload\\template.docx";
 
+            if (System.IO.File.Exists(docxPath))
+            {
+               return await DownloadAsync(docxPath);
+                //using(DocX docx = DocX.Load(docxPath))
+                //{
+                //    docx.ReplaceText("{1}","測試用");
 
+                //}
+            }
 
             return View();
         }
@@ -35,6 +50,20 @@ namespace UseDocx.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public async Task<IActionResult> DownloadAsync(string filePath)
+        {
+            var memoryStream = new MemoryStream();
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memoryStream);
+            }
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            // 回傳檔案到 Client 需要附上 Content Type，否則瀏覽器會解析失敗。
+           return new FileStreamResult(memoryStream, "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
         }
     }
 }
