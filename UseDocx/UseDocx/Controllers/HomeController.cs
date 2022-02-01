55ada7c7096f79d52a8961ad8a6f6c22f3c78acb
jontcont <52921053+JontCont@users.noreply.github.com>
2022-02-01 20:26:03 +0800

    npoi 套版轉換

--- UseDocx/UseDocx/Controllers/HomeController.cs
+++ UseDocx/UseDocx/Controllers/HomeController.cs
@@ -9,7 +9,8 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using UseDocx.Models;
-using Xceed.Words.NET;
+using NPOI.XWPF.UserModel;
+using System.Data;
 
 namespace UseDocx.Controllers
 {
@@ -24,21 +25,73 @@ namespace UseDocx.Controllers
             _env = env;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            string docxPath = _env.WebRootPath+ "\\upload\\template.docx";
+            string docxPath = _env.WebRootPath + "\\upload\\template.docx";
 
             if (System.IO.File.Exists(docxPath))
             {
-               return await DownloadAsync(docxPath);
-                //using(DocX docx = DocX.Load(docxPath))
-                //{
-                //    docx.ReplaceText("{1}","測試用");
+                FileStream fs = new (docxPath, FileMode.Open, FileAccess.Read);
+                XWPFDocument docx = new (fs);
+                foreach (var bodyItem in docx.BodyElements)
+                {
+                    switch (bodyItem.ElementType)
+                    {
+                        case BodyElementType.TABLE:
+                            Set_DocxTableText(bodyItem.Body);
+                            break;
+                        case BodyElementType.PARAGRAPH:
+                            Set_DocxText(bodyItem.Body);
+                            break;
+                        case BodyElementType.CONTENTCONTROL:break;
+                        default:break;
+                    }
+                }
 
-                //}
+                return Download(docx); 
             }
-
             return View();
+        }
+
+        public void Set_DocxText(IBody docx)
+        {
+            foreach (var para in docx.Paragraphs)
+            {
+                string oldtext = para.ParagraphText;
+                string newText = "趙錢孫";
+                if (oldtext == "")
+                    continue;
+                string temptext = para.ParagraphText;
+                //以下為替換文件模版中的關鍵字
+                if (temptext.Contains("[$name$]"))
+                    temptext = temptext.Replace("[$name$]", newText);
+                para.ReplaceText(oldtext, temptext);
+            }
+        }
+
+        public void Set_DocxTableText(IBody docx)
+        {
+            foreach (XWPFTable dt in docx.Tables)
+            {
+                foreach (XWPFTableRow dr in dt.Rows)
+                {
+                    foreach (XWPFTableCell dc in dr.GetTableICells())
+                    {
+                        foreach (var para in dc.Paragraphs)
+                        {
+                            string oldtext = para.ParagraphText;
+                            string newText = "趙錢孫";
+                            if (oldtext == "")
+                                continue;
+                            string temptext = para.ParagraphText;
+                            //以下為替換文件模版中的關鍵字
+                            if (temptext.Contains("[$name$]"))
+                                temptext = temptext.Replace("[$name$]", newText);
+                            para.ReplaceText(oldtext, temptext);
+                        }
+                    }
+                }
+            }
         }
 
         public IActionResult Privacy()
@@ -63,7 +116,16 @@ namespace UseDocx.Controllers
             memoryStream.Seek(0, SeekOrigin.Begin);
 
             // 回傳檔案到 Client 需要附上 Content Type，否則瀏覽器會解析失敗。
-           return new FileStreamResult(memoryStream, "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+            return new FileStreamResult(memoryStream, "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        }
+
+        public IActionResult Download(XWPFDocument fs)
+        {
+            var memoryStream = new MemoryStream();
+            fs.Write(memoryStream);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            // 回傳檔案到 Client 需要附上 Content Type，否則瀏覽器會解析失敗。
+            return new FileStreamResult(memoryStream, "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
         }
     }
 }
